bdc92eb5a99f9ef969d7d6644fa4fb29029e2cba
unknown <ajil7703@gmail.com>
2023-10-31 15:19:12 +0700

    Struktur Folder , Routes dan Views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Nadjrilillah" ,
+        "email" => "muhammadajil7703@gmail.com",
+        "image" => "kaho.jpg"
+
+    ] );
+});
+
+
+Route::get('/post', function () {
+    return view('posts');
 });
